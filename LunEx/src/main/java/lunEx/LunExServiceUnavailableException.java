2fdd101dbaf69bfb55c172ed1321fad4ddd47fd5
Rob Myers <Rob.Myers@agileInstitute.com>
2025-01-06 10:01:05 -0800

    build: Removed surprising SuppressWarnings (I don't recall adding it!) which was seemingly causing a warning. Ironic, yes?

--- LunEx/src/main/java/lunEx/LunExServiceUnavailableException.java
+++ LunEx/src/main/java/lunEx/LunExServiceUnavailableException.java
@@ -1,5 +1,5 @@
 package lunEx;
-@SuppressWarnings("serial")
+
 public class LunExServiceUnavailableException extends RuntimeException {
 	public String getMessage() {
 		return "Sorry, sunspot activity today...please try again later";
